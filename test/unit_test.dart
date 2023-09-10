f8795d8f095047d9f6021efb093cadd7cc906b3a
rohangargbharatnxt <135220684+rohangargbharatnxt@users.noreply.github.com>
2023-09-10 17:23:25 +0530

    Update unit_test.dart

--- test/unit_test.dart
+++ test/unit_test.dart
@@ -7,6 +7,27 @@ void main() {
     await tester.pumpWidget(SimpleFlutterApp());
 
     // Verify that the title and text widgets are present.
+    expect(find.text('BharatNxt Test App'), findsOneWidget);
     expect(find.text('BharatNXT'), findsOneWidget);
   });
+}
+
+class SimpleFlutterApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'BharatNxt Test App',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('BharatNxt Test App'),
+        ),
+        body: Center(
+          child: Text(
+            'BharatNXT',
+            style: TextStyle(fontSize: 24.0),
+          ),
+        ),
+      ),
+    );
+  }
 }
